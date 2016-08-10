976b6d95647570ebf69b24f20fc72941a3f9bfc3
Chandruks <kschandrube@gmail.com>
2016-08-10 15:23:52 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -9,9 +9,19 @@ public class Factorial {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Factorial Number: ");
 		long num = sc.nextLong();
+		if(num>0)
+		{
 		for(int i=1;i<=num;i++)
 			sum = sum*i;
 		System.out.println(sum);
+		}
+		else if(num<0)
+			System.out.println("Enter only positive value ");
+		else if(num==0)
+			{
+			sum = 1;
+			System.out.println(sum);
+			}
 		
 
 	}
