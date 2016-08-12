9ca636e17144ec9beb4d50b6072086c168c445a5
Chandruks <kschandrube@gmail.com>
2016-08-12 09:42:28 +0530

    Create String_Int.java

---
+++ String_Int.java
@@ -0,0 +1,14 @@
+package GUVI_Play;
+import java.util.*;
+public class String_Int 
+{
+	public static void main(String []arg)
+	{
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int num = Integer.parseInt(str);
+		System.out.println("String str: "+str);
+		System.out.println("Integer num: "+num);
+		
+	}
+}
