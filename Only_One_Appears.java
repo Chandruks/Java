d9d182a18ba55650580a6b1d6fb3a9064b2516f0
Chandruks <kschandrube@gmail.com>
2017-06-12 15:41:38 +0530

    Create Only_One_Appears.java

---
+++ Only_One_Appears.java
@@ -0,0 +1,43 @@
+package Hunter;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Only_One_Appears
+{
+	public static void main(String[] args)
+	{
+		int size,i,j,k=0;
+		System.out.println("Enter the Array Size: ");
+		Scanner sc = new Scanner(System.in);
+		size = sc.nextInt();
+		int[] Number = new int[size];
+		String[] tmp = new String[size];
+		System.out.println("Enter the Array Element: ");
+		for(i=0;i<size;i++)
+		{
+			Number[i] = sc.nextInt();
+			tmp[i] = Integer.toString(Number[i]);
+		}
+		for(i=0;i<size;i++)
+		{
+			for(j=i+1;j<size;j++)
+			{
+				if((tmp[i]).equals(tmp[j]))
+				{
+					++k;
+					tmp[j]="*";
+					break;
+				}
+			}
+			if(k!=0)
+				tmp[i]="*";
+			k=0;
+		}
+		for(i=0;i<size;i++)
+		{
+			if(!(tmp[i]).equals("*"))
+				System.out.println("Only onece appear in array: "+Integer.parseInt(tmp[i]));
+		}
+	}
+}
